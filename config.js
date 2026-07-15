95f16c12242ed12b96950387b2b4532352909747
Mirco49 <mirco25994@gmail.com>
2026-07-15 11:40:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
   token: process.env.DISCORD_TOKEN,
   clientId: process.env.CLIENT_ID,
   guildId: process.env.GUILD_ID,
-  adminRoleId: process.env.ADMIN_ROLE_ID || ''
+  adminRoleId: process.env.ADMIN_ROLE_ID || '',
+  adminChannelId: process.env.ADMIN_CHANNEL_ID || '' // Aggiunto per la stanza di controllo
 };
